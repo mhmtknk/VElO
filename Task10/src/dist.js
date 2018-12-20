4029d8031e3704b3a5ca1fceb1950969412dbc19
Mehmet KONUK <mehmetkonuk91@gmail.com>
2018-12-20 22:30:04 +0100

    Task 10
    Webpack

---
+++ Task10/src/dist.js
@@ -0,0 +1,9 @@
+exports.kmConverter = function() {
+    let val = parseFloat(document.getElementById("km").value);
+    document.getElementById("mile").value = (val * 0.62137).toFixed(2);
+}
+
+exports.mileConverter = function() {
+    let val = parseFloat(document.getElementById("mile").value);
+    document.getElementById("km").value = (val / 0.62137).toFixed(2);
+}
